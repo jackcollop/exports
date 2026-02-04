c761f1509398367de50e2d96284e5d33e392e052
jackcollop <jackcollop@gmail.com>
2026-02-04 10:53:43 -0600

    Update lit1.py

--- lit1.py
+++ lit1.py
@@ -1,9 +1,54 @@
 import pandas as pd
 import streamlit as st
 import os
+import numpy as np
 
 key = os.environ['API']
 
 z = pd.read_json(f'https://api.fas.usda.gov/api/esr/countries?api_key={key}')
 
-st.dataframe(z)
+z.countryDescription = z.countryDescription.str.strip(' ') #formatting: removes excess spacing from country names
+
+zz = z.set_index('countryCode')['countryDescription'].to_dict() #creates dictionary to map country codes to names
+#%%
+zz[5700] = 'CHINA'
+zz[5800] = 'KOREA'
+
+year = 2026
+temp = pd.read_json(
+        f'https://api.fas.usda.gov/api/esr/exports/commodityCode/1404/allCountries/marketYear/{year}?api_key={key}')
+temp['Year'] = str(year - 1)
+#%%
+upland = pd.concat([temp, pd.read_csv('upland.csv')])
+
+year = 2026
+temp = pd.read_json(
+    f'https://api.fas.usda.gov/api/esr/exports/commodityCode/1301/allCountries/marketYear/{year}?api_key={key}')
+temp['Year'] = str(year - 1)
+pima = pd.concat([temp, pd.read_csv('pima.csv')])
+
+upland['countryName'] = upland['countryCode'].map(zz)
+pima['countryName'] = pima['countryCode'].map(zz)
+#%%
+
+upland['weekEndingDate'] = pd.to_datetime(upland.weekEndingDate)
+pima['weekEndingDate'] = pd.to_datetime(pima.weekEndingDate)
+
+xp = upland.pivot(index=['Year','weekEndingDate'], columns='countryName', values='weeklyExports')
+
+xp['TOTAL'] = xp.sum(axis=1)
+#%%
+weeks = pd.Series(xp.TOTAL.reset_index().groupby('Year').cumcount() + 1).astype(int)
+#%%
+xp.reset_index(inplace=True)
+#%%
+xp['week'] = weeks.to_numpy()
+#%%
+xp.Year = xp.Year.astype(int)
+#%%
+
+xpp = xp.pivot(index='week', columns='Year', values='TOTAL').drop(columns=[2018,2025]).dropna()
+
+fig = xpp.T.plot(backend='plotly', kind='box')
+
+st.plotly_chart(fig)
