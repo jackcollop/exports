84ab7172a2a74232e8c9e724a78aeb4399dacc2e
jackcollop <jackcollop@gmail.com>
2026-02-05 07:33:18 -0600

    Update lit1.py

--- lit1.py
+++ lit1.py
@@ -66,3 +66,23 @@ xp.reset_index(inplace=True)
 #%%
 xp['week'] = weeks.to_numpy()
 st.dataframe(xp.reset_index().set_index(['Year','week'])[['TOTAL','VIETNAM','CHINA','TURKEY','INDONESIA','MEXICO','INDIA','PAKISTAN','KOREA','BANGLADESH','THAILAND','TAIWAN']].sort_index(ascending=False), width='content')
+
+xp = upland.pivot(index=['Year','weekEndingDate'], columns='countryName', values='weeklyExports')
+xp['TOTAL'] = xp.sum(axis=1)
+#%%
+weeks = pd.Series(xp.TOTAL.reset_index().groupby('Year').cumcount() + 1).astype(int)
+#%%
+xp.reset_index(inplace=True)
+#%%
+xp['week'] = weeks.to_numpy()
+st.dataframe(xp.reset_index().set_index(['Year','week'])[['TOTAL','VIETNAM','CHINA','TURKEY','INDONESIA','MEXICO','INDIA','PAKISTAN','KOREA','BANGLADESH','THAILAND','TAIWAN']].sort_index(ascending=False), width='content')
+
+xp = upland.pivot(index=['Year','weekEndingDate'], columns='countryName', values='weeklyExports')
+xp['TOTAL'] = xp.sum(axis=1)
+#%%
+weeks = pd.Series(xp.TOTAL.reset_index().groupby('Year').cumcount() + 1).astype(int)
+#%%
+xp.reset_index(inplace=True)
+#%%
+xp['week'] = weeks.to_numpy()
+st.dataframe(xp.reset_index().set_index(['Year','week'])[['TOTAL','VIETNAM','CHINA','TURKEY','INDONESIA','MEXICO','INDIA','PAKISTAN','KOREA','BANGLADESH','THAILAND','TAIWAN']].sort_index(ascending=False), width='content')
