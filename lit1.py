e1404f84a5f16d5a2a611a4b007ee94a1d83a6db
jackcollop <jackcollop@gmail.com>
2026-04-14 12:25:57 -0500

    Update lit1.py

--- lit1.py
+++ lit1.py
@@ -88,13 +88,13 @@ weeks = pd.Series(xp.TOTAL.reset_index().groupby('Year').cumcount() + 1).astype(
 xp.reset_index(inplace=True)
 #%%
 xp['week'] = weeks.to_numpy()
-st.caption('Weekly upland shipments')
+st.caption('Total commitment CMY')
 st.dataframe(xp.reset_index().set_index(['Year','week'])[['TOTAL','VIETNAM','CHINA','TURKEY','INDONESIA','MEXICO','INDIA','PAKISTAN','KOREA','BANGLADESH','THAILAND','TAIWAN']].sort_index(ascending=False), width='content')
 
 st.caption('Accumulated upland shipments')
 st.dataframe(xp.reset_index().set_index(['Year','week'])[['TOTAL','VIETNAM','CHINA','TURKEY','INDONESIA','MEXICO','INDIA','PAKISTAN','KOREA','BANGLADESH','THAILAND','TAIWAN']].sort_index(ascending=False), width='content')
 
-xp = upland.pivot(index=['Year','weekEndingDate'], columns='countryName', values='accumulatedExports')
+xp = upland.pivot(index=['Year','weekEndingDate'], columns='countryName', values='currentMYTotalCommitment')
 xp['TOTAL'] = xp.sum(axis=1)
 #%%
 weeks = pd.Series(xp.TOTAL.reset_index().groupby('Year').cumcount() + 1).astype(int)
