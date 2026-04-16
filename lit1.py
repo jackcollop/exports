fbd21ae7da2ced92dc2fce925c0758565480843b
jackcollop <jackcollop@gmail.com>
2026-04-16 10:18:23 -0500

    Update lit1.py

--- lit1.py
+++ lit1.py
@@ -48,7 +48,7 @@ xp['week'] = weeks.to_numpy()
 # xp.Year = xp.Year.astype(int)
 #%%
 
-xpp = xp.pivot(index='week', columns='Year', values='TOTAL').drop(columns=['2025']).dropna()
+xpp = xp.pivot(index='week', columns='Year', values='TOTAL')[['2020','2021','2022','2023','2024']].dropna()
 
 fig = px.box(xpp.T)
 
